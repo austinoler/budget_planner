45c31141eb7c389cfa0936b652a32066d60b3205
Nainoa Dinson <nd808@msn.com>
2024-02-22 01:15:28 -1000

    created budget table

--- client/src/components/BudgetTable/index.jsx
+++ client/src/components/BudgetTable/index.jsx
@@ -1,8 +1,41 @@
 function BudgetTable() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <table className="table">
+      <thead>
+        <tr className="col">
+        <th scope="col" className="w-25">Category</th>
+        <th scope="col" className="w-25">Budget $</th>
+        <th scope="col" className="w-25">Expenses $</th>
+        <th scope="col" className="w-25">Budget Variance $</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Housing</th>
+        <td>0000</td>
+        <td>0000</td>
+        <td>0000</td>
+      </tr>
+      <tr>
+        <th scope="row">Food</th>
+        <td>0000</td>
+        <td>0000</td>
+        <td>0000</td>
+      </tr>
+      <tr>
+        <th scope="row">Transportation</th>
+        <td>0000</td>
+        <td>0000</td>
+        <td>0000</td>
+      </tr>
+      <tr>
+        <th scope="row">Misc.</th>
+        <td>0000</td>
+        <td>0000</td>
+        <td>0000</td>
+      </tr>
+    </tbody>
+  </table>
   );
 };
 
--- client/src/components/BudgetTotal/index.jsx
+++ client/src/components/BudgetTotal/index.jsx
@@ -5,14 +5,15 @@ function BudgetTotal() {
   };
 
   return (
-    <div className="form">
-      <div className="form-Group">
+    <div className="form justify-content-center">
+      <div className="form-Group text-center">
         <label for="budgetInput">This Month's Budget:</label>
-        <div className="row">
-          <span className="col-2">
+        <div className="row justify-content-center">
+          <span className="col-1 w-auto fs-4">$</span>
+          <span className="col-2 w-25">
             <input type="number" min="0" step="any" onchange="setTwoPlaceDecimal" className="form-control" id="inputBudget" placeholder="Enter Amount"></input>
           </span>
-          <span className="col-1">
+          <span className="col-1 w-auto">
             <i className="bi bi-pencil-square"></i>
           </span>
         </div> 
