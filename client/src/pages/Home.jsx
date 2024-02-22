6405d56511712bb6bf4e39c6808f3c685972d7b2
Nainoa Dinson <nd808@msn.com>
2024-02-21 15:48:51 -1000

    created component files for BudgetTotal, BudgetTable, ExpensesForm, TransactionsTable

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,32 @@
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+// import ProductList from "../components/ProductList";
+// import CategoryMenu from "../components/CategoryMenu";
+// import Cart from "../components/Cart";
+
+// const Home = () => {
+//   return (
+//     <div className="container">
+//       <CategoryMenu />
+//       <ProductList />
+//       <Cart />
+//     </div>
+//   );
+// };
+
+
+import BudgetTotal from "../components/BudgetTotal";
+import BudgetTable from "../components/BudgetTable";
+import ExpensesForm from "../components/ExpensesForm";
+import TransactionsTable from "../components/TransactionsTable";
 
 const Home = () => {
   return (
     <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
+      <BudgetTotal/>
+      <BudgetTable/>
+      <ExpensesForm/>
+      <TransactionsTable/>
     </div>
-  );
-};
+  )
+}
 
 export default Home;
