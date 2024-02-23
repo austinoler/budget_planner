dd39dbf9bfc6f1ec9652d81db412db67ba3eea60
Nainoa Dinson <nd808@msn.com>
2024-02-22 16:50:13 -1000

    commit merge changes

--- client/src/components/BudgetTable/index.jsx
+++ client/src/components/BudgetTable/index.jsx
@@ -1,4 +1,11 @@
 function BudgetTable() {
+  function housingBudgetVariance() { 
+    var housingBudget = parseInt(document.getElementById("housing-budget").value);
+    var housingExpense = parseInt(document.getElementById("housing-expense").value);
+ 
+    var housingVariance = document.getElementById("housing-variance");
+      housingVariance.value = housingBudget - housingExpense;
+  };
   return (
   // Table that will display user's budget categorie/ amount, expenses, and budget variance  
     <table className="table">
@@ -13,27 +20,31 @@ function BudgetTable() {
     <tbody>
       <tr>
         <th scope="row">Housing</th>
-        <td>0000</td>
-        <td>0000</td>
-        <td>0000</td>
+        < div className= "form-group">
+          <input type="number" name="housing-budget-input" id="housing-budget"></input>
+        </div>
+        <td id="housing-expenses" onchange="housingBudgetVariance()">50</td>
+        <td id="budget-variance">0000</td>
       </tr>
       <tr>
         <th scope="row">Food</th>
-        <td>0000</td>
-        <td>0000</td>
-        <td>0000</td>
+        < div className= "form-group">
+          <input type="number" name="food-budget-input" id="food-budget"></input>
+        </div>
+        <td id="food-expenses">0000</td>
+        <td id="food-variance">0000</td>
       </tr>
       <tr>
         <th scope="row">Transportation</th>
-        <td>0000</td>
-        <td>0000</td>
-        <td>0000</td>
+        <td id="transportation-budget">0000</td>
+        <td id="transportation-expenses">0000</td>
+        <td id="transportation-variance">0000</td>
       </tr>
       <tr>
         <th scope="row">Misc.</th>
-        <td>0000</td>
-        <td>0000</td>
-        <td>0000</td>
+        <td id="misc-budget">0000</td>
+        <td id="misc-expenses">0000</td>
+        <td id="misc-variance">0000</td>
       </tr>
     </tbody>
   </table>
