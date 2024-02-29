e593a34ddcd592d1a7c48dcace3a3206ec406453
Nainoa Dinson <nd808@msn.com>
2024-02-29 01:05:08 -1000

    added background and styling to budgets page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -42,10 +42,28 @@ function Dashboard() {
     }
 
     return (
-        <div className="d-flex flex-column align-items-center">
-            <div>Hello {user}</div>
-            <div>My Budgets</div>
-            <BudgetList budgets={data.user.budgets}></BudgetList>
+        <div className="bg-image justify-content-center opacity-75 p-4 border border-1 border-success rounded p-4 shadow"
+        style={{
+          backgroundImage: "url(/assets/images/dashboard-bg.jpg)",
+          height: "100vh",
+          width: "100vw",
+          bakgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          webkitBackgroundSize: 'cover',
+          mozBackgroundSize: 'cover',
+          oBackgroundSize: 'cover',
+
+        }}>
+        
+            <div className="row align-items-center row w-100 justify-content-center mt-4 p-4">
+                <h1 className= "col-3 bg-white text-center rounded bolder shadow border border-1 border-success">Hello {user}!</h1>
+                <div className= "col-2"></div>
+                <div className="col-5 border border-1 border-success rounded p-4 shadow bg-light">
+                <h2 className="border border-1 border-success rounded text-center"><i className="bi bi-cash"></i> MY BUDGETS</h2>
+                <BudgetList budgets={data.user.budgets}></BudgetList>
+                </div>     
+            </div>
+
         </div>
     )
 
