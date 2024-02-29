789a9dbacf7ff62b0305270829bf7f0ea03075c9
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2024-02-28 20:02:46 -0500

    When the user adds an expense it is added to the database

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,7 +8,7 @@ import Signup from './Signup';
 import Login from './Login';
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_BUDGET } from '../utils/queries';
-import { ADD_BUDGET } from '../utils/mutations';
+import { ADD_BUDGET, ADD_EXPENSE } from '../utils/mutations';
 import { useParams } from 'react-router-dom';
 import { getExpenses } from '../utils/helpers';
 
@@ -16,22 +16,35 @@ const Home = () => {
 
   const { id } = useParams();
   //   console.log('id: ', id);
+  const [addExpense] = useMutation
+  (ADD_EXPENSE
+    // , {
+    // refetchQueries: [
+    //   QUERY_BUDGET,
+    //   'getBudget'
+    // ]
+  // }
+  );
+
 
   const { loading, data } = useQuery(QUERY_BUDGET, {
     variables: { id },
   });
 
-  // console.log('data: ', data.budget.total);
-  // const categoriesData = data.budget.categories
-
-  // const expensesData = getExpenses(categoriesData);
-  // console.log('categories', categories);
-  // console.log('expenses', expensesData);
-
+  if (!Auth.loggedIn()) {
+    return (
+      <div className="row shadow rounded border border-3 p-4">
+        <Login />
+        <h2 className="col-2 align-self-center">OR</h2>
+        <Signup />
+      </div>
+    );
+  }
 
   // Check if the current loggedin user has a budget for this month. If not, create one with default amount of 500
   var userId = Auth.getProfile().data._id
   const currentDate = new Date;
+  const day = currentDate.getDay();
   const month = currentDate.getMonth() + 1;
   const year = currentDate.getFullYear();
 
@@ -39,7 +52,21 @@ const Home = () => {
   useEffect(() => {
     if (data) {
       var housingExpenses = 0, foodExpenses = 0, transportationExpenses = 0, miscExpenses = 0;
+      var housingID, foodID, transportationID, miscID;
       const categoriesData = data.budget.categories
+
+      categoriesData.forEach(category => {
+        if(category.name == 'Housing'){
+          housingID = category._id
+        } else if(category.name == 'Food'){
+          foodID = category._id
+        } else if(category.name == 'Transportation'){
+          transportationID = category._id
+        } else if(category.name == 'Misc'){
+          miscID = category._id
+        }
+        setcategoryIDs({housingID, foodID, transportationID, miscID})
+      })
       const expensesData = getExpenses(categoriesData)
       setExpenses([...expenses, ...expensesData]);
       console.log('expenses data:', expensesData);
@@ -59,6 +86,7 @@ const Home = () => {
   }, [data]
   );
 
+  // state variable used to store the total expenses for each category
   const [expensesByCat, setExpensesByCat] = useState({
     Housing: '',
     Food: '',
@@ -66,8 +94,34 @@ const Home = () => {
     Misc: ''
   });
 
+  const [categoryIDs, setcategoryIDs] = useState({
+    housingID: '',
+    foodID: '',
+    transportationID: '',
+    miscID: ''
+  });
+
   const [expenses, setExpenses] = useState([]);
-  const handleExpenseSubmit = (expense) => {
+  const handleExpenseSubmit = async (expense) => {
+    var categoryId = '';
+    if(expense.categoryName == 'Housing'){
+      categoryId = categoryIDs.housingID
+    }else if(expense.categoryName == 'Food'){
+      categoryId = categoryIDs.foodID
+    }else if(expense.categoryName == 'Transportation'){
+      categoryId = categoryIDs.transportationID
+    }else if (expense.categoryName == 'Misc'){
+      categoryId = categoryIDs.miscID
+    }
+    const {data} = await addExpense({
+      variables: {
+        userId,
+        categoryName: expense.categoryName, 
+        day, month, year, amount: parseFloat(expense.amount),
+         description: expense.description,
+          categoryId
+      }
+    });
     setExpenses([...expenses, expense]);
   };
 
